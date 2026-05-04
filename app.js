ccdf61fb638fc0139a5e1086e2cbe68f34d339a2
canda <candakurniawan4241@gmail.com>
2026-05-04 20:49:04 +0700

    Update response message and log output in app.js

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Deploy Berhasil!</h1><p>Aplikasi Node.js ini berjalan via Jenkins & ArgoCD.</p>');
+  res.send(`
+    <div style="text-align: center; font-family: sans-serif; margin-top: 50px;">
+      <h1>🚀 Deploy Berhasil!</h1>
+      <p>Aplikasi Node.js ini berjalan di Kubernetes via Jenkins & ArgoCD.</p>
+      <p>Identifier: <b>kowallskii</b> | Repo: <b>poinpik-apps</b></p>
+    </div>
+  `);
 });
 
 app.listen(port, () => {
-  console.log(`App running at http://localhost:${port}`);
+  console.log(`Server jalan di port ${port}`);
 });
