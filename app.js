fba1b679fce8498c0cebeb8a1f39ae2b5857cf45
canda <candakurniawan4241@gmail.com>
2026-05-19 21:33:59 +0700

    Update heading text in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send(`
     <div style="text-align: center; font-family: sans-serif; margin-top: 50px;">
-      <h1>🚀 Mememek JS Terbang Tinggi</h1>
+      <h1>🚀 PointPik JS Terbang Tinggi</h1>
       <p>Aplikasi Node.js ini berjalan di Kubernetes via Jenkins & ArgoCD.</p>
       <p>Identifier: <b>kowallskii</b> | Repo: <b>poinpik-apps</b></p>
     </div>
